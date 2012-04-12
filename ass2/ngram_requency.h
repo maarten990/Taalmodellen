23ec08784f468acdd1456be94cd5856415c14b0c
Maarten de Jonge <mdejonge1990@gmail.com>
2012-04-12 13:57:22 +0200

    added assignment2 folder

---
+++ ass2/ngram_requency.h
@@ -0,0 +1,19 @@
+#ifndef NGRAM_REQUENCY_H
+#define NGRAM_REQUENCY_H
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <string>
+
+#include <boost/regex.hpp>
+
+using namespace std;
+
+// function declarations
+map<string, int> ngram_frequencies(char file_location[], int n);
+vector<string> get_lines(char file_location[]);
+void add_symbols(vector<string> &lines, int n);
+
+#endif // NGRAM_REQUENCY_H
