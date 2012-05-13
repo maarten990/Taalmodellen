ba1f9dedf252dff344d99e69fc1c4be24b76811b
Inge <inge.becht91@gmail.com>
2012-05-13 13:58:46 +0200

    top 5 letters belonging to a c ipher letter function made

--- crypto/main.cpp
+++ crypto/main.cpp
@@ -42,6 +42,80 @@ void file_to_lowercase(string pathname, bool corpus){
 }
 
 
+// We make a map that consists of the top 5 mapping
+// belonging to a letter in cipher
+map<string, string> calculate_top_5(map<string, double> cipher, map<string, double> corpus)
+{
+    map<string, string> top_5;
+    double temp;
+    double top1 = 0, top2 = 0, top3 = 0, top4 = 0, top5 = 0;
+    string top1c = "-",top2c = "-", top3c = "-",top4c = "-", top5c = "-";
+    for(auto &element: cipher)
+    {
+        for(auto &element2: corpus)
+        {
+            temp = 1 - abs(element.second - element2.second);
+
+            if(1 - abs(element.second - element2.second) > top1)
+            {
+                top5 = top4;
+                top5c= top4c;
+                top4 = top3;
+                top4c= top3c;
+                top3 = top2;
+                top3c= top2c;
+                top2 = top1;
+                top2c= top1c;
+                top1 = temp;
+                top1c = element2.first;
+            }
+            else if(temp > top2 )
+            {
+                top5 = top4;
+                top5c = top4c;
+                top4 = top3;
+                top4c = top3c;
+                top3 = top2;
+                top3c = top2c;
+                top2 = temp;
+                top2c = element2.first;
+            }
+            else if(temp > top3 )
+            {
+                top5 = top4;
+                top5c = top4c;
+                top4 = top3;
+                top4c = top3c;
+                top3 = temp;
+                top3c = element2.first;
+            }
+            else if(temp > top4 )
+            {
+                top5 = top4;
+                top5c = top4c;
+                top4 = temp;
+                top4c = element2.first;
+            }
+            else if(temp > top5 )
+            {
+                top5 = temp;
+                top5c = element2.first;
+            }
+        }
+        // Now that we have a top 5 we insert it
+        // in our map at the right location
+        top_5[element.first] = top1c + top2c + top3c + top4c + top5c;
+        top1 = 0;
+        top2 = 0;
+        top3 = 0;
+        top4 = 0;
+        top5 = 0;
+    }
+    return top_5;
+
+}
+
+
 int main(int argc, char *argv[])
 {
 
@@ -75,5 +149,11 @@ int main(int argc, char *argv[])
         }
     }
 
+    map<string, string> bla = calculate_top_5(f.cipher, f.corpus);
+
+    for (auto &pair : bla)
+    {
+        std::cout<<pair.first<<" : "<< pair.second<<std::endl;
+    }
 
 }
